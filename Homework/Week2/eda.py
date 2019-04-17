d044371baec65b2cd3ba67162607f3f178cc9bf9
Djenzenpan <jmppannekeet@gmail.com>
2019-04-17 21:19:16 +0200

    Week 2 fin

--- Homework/Week2/eda.py
+++ Homework/Week2/eda.py
@@ -3,32 +3,32 @@
 # Student number: 10151494
 from functions import *
 """
-this script opens csv file and converts it to a panda dataframe
+opens csv file, filters into panda dataframe according to 'filter', finds
+some describing values and plots some of the data in the dataframe associated
+with gdp and mortality
+saves dataframe as json file in path specified by loc
 """
-
 gdp = "GDP ($ per capita) dollars"
 mortality = "Infant mortality (per 1000 births)"
 filter = ["Country", "Region", "Pop. Density (per sq. mi.)",
           "Infant mortality (per 1000 births)", "GDP ($ per capita) dollars"]
+loc = r"D:\Psychobiologie\Mprog\Datavisualisatie\Homework\week2\output.json"
 
 if __name__ == "__main__":
-    ### Reading and preprocessing data ###
+    # Reading and preprocessing data
     table = process_table(pandas.read_csv("input.csv", decimal=','), filter)
 
-    ### Prints some of the common measures of GDP ###
+    # Prints some of the common measures of GDP
     common_measures(table, gdp)
 
-    ### Histogram of GDP ###
+    # Histogram of GDP
     plot_hist(table, gdp)
 
-    ### Prints five number summary of mortality rate ###
+    # Prints five number summary of mortality rate
     five_numbers(table, mortality)
 
-    ### Boxplot of mortality rate ###
+    # Boxplot of mortality rate
     plot_boxp(table, mortality)
 
-    ### Converts panda dataframe to JSON file with country name as index ###
-    conv_to_json(table, "Country", r"D:\Psychobiologie\Mprog\Datavisualisatie\Homework\week2\output.json")
-
-    ### Scatterplot ###
-    plot_scatter(table, gdp, mortality)
+    # Converts panda dataframe to JSON file with country name as index
+    conv_to_json(table, "Country", loc)
--- Homework/Week2/functions.py
+++ Homework/Week2/functions.py
@@ -6,74 +6,85 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+from pandas.api.types import is_numeric_dtype
 
 
-def process_table(data):
+# processes panda database based on filter
+def process_table(data, filter):
+    # keeps wanted parts of table
     data = data.filter(items=filter)
-    data["Pop. Density (per sq. mi.)"].replace("unknown", np.nan, inplace=True)
-    data["GDP ($ per capita) dollars"].replace("unknown", np.nan, inplace=True)
+
+    # removes 'unknown' and empty values
+    data[filter[2]].replace("unknown", np.nan, inplace=True)
+    data[filter[4]].replace("unknown", np.nan, inplace=True)
     data.dropna(axis=0, how='any', inplace=True)
-    data["Region"] = data["Region"].str.strip()
-    data["GDP ($ per capita) dollars"] = data["GDP ($ per capita) dollars"].map(lambda x: x.rstrip(" dollars"))
-    data["GDP ($ per capita) dollars"] = pandas.to_numeric(data["GDP ($ per capita) dollars"])
-    data["Infant mortality (per 1000 births)"] = pandas.to_numeric(data["Infant mortality (per 1000 births)"])
+
+    # strips away unecessary characters
+    data[filter[1]] = data[filter[1]].str.strip()
+    data[filter[4]] = data[filter[4]].map(lambda x: x.rstrip(" dollars"))
+
+    # makes data numeric
+    data[filter[4]] = pandas.to_numeric(data[filter[4]])
+    data[filter[3]] = pandas.to_numeric(data[filter[3]])
+
+    # removes outliers
+    data = remove_outlier(data)
+
+    # returns clean file
     return(data)
 
 
-def common_measures(data, string):
-    print("Mean, median, mode and standard deviation of GDP ($ per capita) for countries around the world:")
-    print("Mean: " + str(round(np.mean(data[string]), 1)))
-    print("Median: " + str(np.median(data[string])))
-    print("Mode: " + str(data.mode(axis=0)[string][0]))
-    print("Standard deviation: " + str(round(np.std(data[string]), 1)))
+# from https://gist.github.com/ariffyasri/70f1e9139da770cb8514998124560281
+def remove_outlier(data):
+    quant_data = data.quantile([0.995])
+    for column in list(data.columns):
+        if is_numeric_dtype(data[column]):
+            data = data[(data[column] < quant_data.loc[0.995, column])]
+    return data
+
+
+# prints some common values associated with a set of numeric data
+def common_measures(data, column):
+    print("Mean, median, mode and standard deviation of GDP ($ per capita)"
+          "for countries around the world")
+    print("Mean: " + str(round(np.mean(data[column]), 1)))
+    print("Median: " + str(np.median(data[column])))
+    print("Mode: " + str(data.mode(axis=0)[column][0]))
+    print("Standard deviation: " + str(round(np.std(data[column]), 1)))
     print("")
 
 
-def plot_hist(data, string):
-    plt.hist(list(data[string]),bins=200,density=False)
+# plots histogram of numeric data
+def plot_hist(data, column):
+    plt.hist(list(data[column]), bins=20, density=False, alpha=0.5,
+             histtype='bar', ec='black')
     plt.title("Histogram of countries' GDP ($ per capita) in dollars")
-    plt.xlabel("GDP ($ per capita)")
+    plt.xlabel(column)
     plt.ylabel("Number of countries")
     plt.show()
 
 
-def five_numbers(data, string):
-    print("Minimum, 1st qaurtile, median, 3rd qaurtile and Maximum of infant mortality (per 1000 births) for countries around the world:")
-    print("Minimum: " + str(np.min(data[string])))
-    print("First qaurtile: " + str(np.percentile(data[string], 25)))
-    print("Median: " + str(np.median(data[string])))
-    print("3rd qaurtile: " + str(np.percentile(data[string], 75)))
-    print("Maximum: " + str(np.max(data[string])))
+# prints some common values associated with a set of numeric data
+def five_numbers(data, column):
+    print("Minimum, 1st qaurtile, median, 3rd qaurtile and Maximum of infant "
+          "mortality (per 1000 births) for countries around the world")
+    print("Minimum: " + str(np.min(data[column])))
+    print("First qaurtile: " + str(np.percentile(data[column], 25)))
+    print("Median: " + str(np.median(data[column])))
+    print("3rd qaurtile: " + str(np.percentile(data[column], 75)))
+    print("Maximum: " + str(np.max(data[column])))
 
 
-def plot_boxp(data, string):
-    plt.boxplot(data[string])
+# plots boxplot of numeric data
+def plot_boxp(data, column):
+    plt.boxplot(data[column])
     plt.title("Boxplot of countries' infant mortality (per 1000 births)")
-    plt.ylabel("Infant mortality (per 1000 births)")
+    plt.ylabel(column)
+    plt.xticks([], "")
     plt.show()
 
 
-def conv_to_json(data, index, string):
+# converts panda database to json file
+def conv_to_json(data, index, save_location):
     data.set_index(index, inplace=True)
-    data.to_json(string, orient='index')
-
-
-def plot_scatter(data, observation_one, observation_two):
-    regions = []
-    for region in data["Region"]:
-        if region not in regions:
-            regions.append(region)
-    colours = ["k", "b", "r", "y", "g", "gray", "m", "turquoise","saddlebrown", "c", "mediumpurple"]
-
-    for region, colour in zip(regions, colours):
-        data = data[data.Region == region]
-        plt.scatter(data[observation_one], data[observation_two], label=region,
-                    s=2, c=colour, marker="s")
-    plt.show()
-
-    ### regression? ###
-    print(data[observation_two])
-    m,b = np.polyfit(data[observation_one],
-                     data[observation_two], 1)
-    plt.plot(b*data[observation_two]+m)
-    plt.show()
+    data.to_json(save_location, orient='index')
