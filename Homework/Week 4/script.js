491a6a225cd96110f27837c2748edf77ed2ce986
Djenzenpan <jmppannekeet@gmail.com>
2019-05-07 22:30:14 +0200

    X-axis

--- Homework/Week 4/script.js
+++ Homework/Week 4/script.js
@@ -1,31 +1,56 @@
 var fileName = "average_data.json";
 var txtFile = new XMLHttpRequest();
 
-// Sets width and height of SVG object
-var w = 1000;
-var h = 500;
+var pageTitle = "Averaged value of all countries for every year between 1960-2016"
 
-var distanceBtwnBars = 17
+// Sets width and height of SVG object
+var w = 1150;
+var h = 420;
+
+var barWidth = 15
+var distanceBtwnBars = barWidth + 3
 var reductionFactor = 40
 
-// Implements basics of html page
+// Implements basics(title, text and name) of index.html page
 function basics() {
-  d3.select("head").append("title").text("Page title");
-  d3.select("body").append("h1").text("Bar chart title").style("color", "red");
-  d3.select("body").append("a").text("Jesse Pannekeet");
-  d3.select("body").append("br");
-  d3.select("body").append("a").text("10151494");
+
+  // Sets page title
+  d3.select("head").append("title").text("Average value/year for every country");
+
+  // Displays title of body and sets font
+  d3.select("body").append("h2").text(pageTitle)
+    .style("text-decoration", "underline")
+    .style("font-family", "Monospace");
+
+  // Displays explanatory text
   d3.select("body").append("p").text("A bit of text");
+
+  // Displays name and student number and sets font for both
+  d3.select("body").append("a").text("Jesse Pannekeet");
+  d3.select("body").append("br")
+  d3.select("body").append("a").text("10151494");
+  d3.selectAll("a").style("font-family", "Monospace")
+  d3.select("body").append("br")
+
+  // Sets whole body to center of page
+  d3.select("body").style("text-align", "center");
 }
 
 function convertToBars(datafile) {
   var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+  var scale = d3.scaleLinear()
+                .domain([0, d3.max(datafile, function(d) { return d; })])
+                .range([0, h]);
+  var xAxis = d3.axisBottom(d3.scaleLinear()
+                .domain([1960, 2017])
+                .range([15, 1037]))
+
   // scale???
   svg.selectAll("rect").data(datafile).enter().append("rect")
-    .style("width", "15")
-    .style("height", function(d){ return (d); })
-    .style("x", function(d, i){ return (i * distanceBtwnBars); })
-    .style("y", function(d){ return h - 10 - d / reductionFactor; } )
+    .style("width", barWidth)
+    .style("height", function(d){ return scale(d); })
+    .style("x", function(d, i){ return (i * distanceBtwnBars) + 30; })
+    .style("y", function(d){ return h - 20 - scale(d); } )
     .attr("fill", function(d, i){
       if (i % 2 == 0) {
         return "black";
@@ -35,15 +60,15 @@ function convertToBars(datafile) {
       }
     })
     .attr("class", function(d, i){ return i + 1960; });
-
+    svg.append("g").call(xAxis).attr("transform", "translate(15, 400)");
 }
 
 txtFile.onreadystatechange = function() {
   if (txtFile.readyState === 4 && txtFile.status == 200) {
-    // Opens JSON file, screens and averages the data for each day
+    // Opens clean JSON file and converts it to a bar graph
     datafile = JSON.parse(txtFile.responseText)
-    convertToBars(datafile);
     basics();
+    convertToBars(datafile);
   }
 }
 txtFile.open("GET", fileName);
