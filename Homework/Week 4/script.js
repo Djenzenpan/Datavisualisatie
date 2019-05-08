bb2b5c120b12c80a5bedd8b6e2a118251473886b
Djenzenpan <jmppannekeet@gmail.com>
2019-05-09 00:43:16 +0200

    Complete

--- Homework/Week 4/script.js
+++ Homework/Week 4/script.js
@@ -10,8 +10,12 @@ var barWidth = 15;
 var distanceBtwnBars = barWidth + 3;
 
 // Remembers graph title and first year
-var graphTitle = "Averaged value of all countries for every year between 1960-2016"
-var firstYear = 1960
+var graphText = "This bar chart contains averaged data of all countries' \
+                 value for each year between 1960 and 2016. \
+                 Data comes from the OECD";
+var graphTitle = "Averaged value of all countries for every \
+                  year between 1960-2016";
+var firstYear = 1960;
 
 // Implements basics(title, text and name) of index.html page
 function basics() {
@@ -25,14 +29,14 @@ function basics() {
     .style("font-family", "Monospace");
 
   // Displays explanatory text
-  d3.select("body").append("p").text("A bit of explanatory text");
+  d3.select("body").append("p").text(graphText);
 
   // Displays name and student number and sets font for both
   d3.select("body").append("a").text("Jesse Pannekeet");
-  d3.select("body").append("br")
+  d3.select("body").append("br");
   d3.select("body").append("a").text("10151494");
-  d3.selectAll("a").style("font-family", "Monospace")
-  d3.select("body").append("br")
+  d3.selectAll("a").style("font-family", "Monospace");
+  d3.select("body").append("br");
 
   // Sets whole body to center of page
   d3.select("body").style("text-align", "center");
@@ -46,43 +50,47 @@ function convertToBarGraph(datafile) {
 
   // Sets scale
   var scale = d3.scaleLinear()
-                .domain([0, d3.max(datafile, function(d) { return d + 20; })])
+                .domain([0, d3.max(datafile, function(d) { return d; })])
                 .range([0, h - 50]);
-  var xAxis = d3.axisBottom(d3.scaleLinear()
-                            .domain([firstYear, 2017])
-                            .range([15, 1037]))
+
+  // Sets x and y axis range
+  var xAxis = d3.axisBottom(d3.scaleLinear().domain([firstYear, 2017])
+                              .range([15, 1037]));
   var yAxis = d3.axisLeft(d3.scaleLinear()
-                            .domain([0, d3.max(datafile, function(d) {return d })])
-                            .range([400, 0]))
+                            .domain([0, d3.max(datafile, function(d) {
+                                                         return d; })])
+                            .range([400, 0]));
 
   // Plots bar graph in svg element in index.html
   svg.selectAll("rect").data(datafile).enter().append("rect")
-     .style("width", barWidth)
-     .text(function(d){ return d })
-     .style("height", function(d){ return scale(d); })
-     .style("x", function(d, i){ return (i * distanceBtwnBars) + 50; })
-     .style("y", function(d){ return h - 50 - scale(d); } )
-     .attr("fill", function(d, i){
-       if (i % 2 == 0) { return "black"; }
-       else { return "green" }
+
+    // Sets size and location of bars
+    .style("width", barWidth)
+    .style("height", function(d){ return scale(d); })
+    .style("x", function(d, i){ return (i * distanceBtwnBars) + 50; })
+    .style("y", function(d){ return h - 50 - scale(d); } )
+
+    // Sets colour of bars to alternate
+    .attr("fill", function(d, i){
+     if (i % 2 == 0) { return "black"; }
+     else { return "green"; }
     })
     // Changes bar colour and displays tooltip on mousehover
-    .on('mouseover', function (d, i) {
-        d3.select(this).transition().duration('50').attr('opacity', '.50')
-        d3.select("#A" + (i+firstYear)).transition().attr('opacity', '1')
-      })
-      // Changes bar colour and stops dispaying tooltip when mousehover stops
-    .on('mouseout', function (d, i) {
-        d3.select(this).transition().duration('50').attr('opacity', '1')
-        d3.select("#A" + (i+firstYear)).transition().attr('opacity', '0')
-
+    .on("mouseover", function (d, i) {
+      d3.select(this).transition().duration("50").attr("opacity", ".5")
+      d3.select("#A" + (i+firstYear)).transition().attr("opacity", "1")
+    })
+    // Changes bar colour and stops dispaying tooltip when mousehover stops
+    .on("mouseout", function (d, i) {
+      d3.select(this).transition().duration("50").attr("opacity", "1")
+      d3.select("#A" + (i+firstYear)).transition().attr("opacity", "0")
     });
 
   // Sets tooltips
   svg.selectAll("text").data(datafile).enter().append("text")
      .text(function(d,i){ return parseInt(d)} )
-     .attr("fill", "red")
-     .attr('opacity', '0')
+     .attr("fill", "blue")
+     .attr("opacity", "0")
      .attr("id", function(d, i){ return "A" + (i + firstYear); })
      .attr("transform", function(d,i){
             return "translate(" + (40 + (i*distanceBtwnBars)) + "," +
@@ -91,9 +99,9 @@ function convertToBarGraph(datafile) {
 
   // Plots axes
   svg.append("g").attr("class", "axis").call(xAxis)
-     .attr("transform", "translate(40, 400)");
+     .attr("transform", "translate(35, 400)");
   svg.append("g").attr("class", "axis").call(yAxis)
-     .attr("transform", "translate(55, 0)");
+     .attr("transform", "translate(50, 0)");
 
   // Plots axes labels
   svg.append("text").attr("transform", "translate(578, 435)")
@@ -101,7 +109,7 @@ function convertToBarGraph(datafile) {
      .text("Every year between 1960-2016");
   svg.append("text").attr("transform", "rotate(-90)").attr("y", 12)
      .attr("x", -175).style("text-anchor", "middle")
-     .style("font-family", "Arial").text("Average value");
+     .style("font-family", "Arial").text("Average value ($)");
 }
 
 
