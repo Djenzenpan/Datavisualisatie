f3789a2c5021c2db5f094e9cf429fda3b166e871
Djenzenpan <jmppannekeet@gmail.com>
2019-05-14 16:45:02 +0200

    3 datasets with corresponding countries

--- Homework/week_5/scatter.js
+++ Homework/week_5/scatter.js
@@ -4,9 +4,9 @@
 
 window.onload = function() {
   // Loads datasets and converts them to json files
-  var teensInViolentArea = "https://stats.oecd.org/SDMX-JSON/data/CWB/AUS+AUT+BEL+BEL-VLG+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+OAVG+NMEC+BRA+BGR+CHN+COL+CRI+HRV+CYP+IND+IDN+MLT+PER+ROU+RUS+ZAF.CWB11/all?startTime=2010&endTime=2017"
-  var teenPregnancies = "https://stats.oecd.org/SDMX-JSON/data/CWB/AUS+AUT+BEL+BEL-VLG+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+OAVG+NMEC+BRA+BGR+CHN+COL+CRI+HRV+CYP+IND+IDN+MLT+PER+ROU+RUS+ZAF.CWB46/all?startTime=2010&endTime=2017"
-  var gdp = "https://stats.oecd.org/SDMX-JSON/data/SNA_TABLE1/AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+EU28+EU15+OECDE+OECD+OTF+NMEC+ARG+BRA+BGR+CHN+COL+CRI+HRV+CYP+IND+IDN+MLT+ROU+RUS+SAU+ZAF+FRME+DEW.B1_GE.HCPC/all?startTime=2010&endTime=2017&dimensionAtObservation=allDimensions"
+  var teensInViolentArea = "https://stats.oecd.org/SDMX-JSON/data/CWB/AUT+BEL+BEL-VLG+CZE+DNK+EST+FIN+FRA+GRC+HUN+ISL+IRL+ITA+LVA+LTU+LUX+NLD+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+GBR+NMEC+BRA+BGR+HRV+CYP+MLT+PER+ROU.CWB11/all?startTime=2010&endTime=2015"
+  var teenPregnancies = "https://stats.oecd.org/SDMX-JSON/data/CWB/AUT+BEL+BEL-VLG+CZE+DNK+EST+FIN+FRA+GRC+HUN+ISL+IRL+ITA+LVA+LTU+LUX+NLD+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+GBR+NMEC+BRA+BGR+HRV+CYP+MLT+PER+ROU.CWB46/all?startTime=2010&endTime=2015"
+  var gdp = "https://stats.oecd.org/SDMX-JSON/data/SNA_TABLE1/AUT+BEL+CZE+DNK+EST+FIN+FRA+GRC+HUN+ISL+IRL+ITA+LVA+LTU+LUX+NLD+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+GBR+BGR+HRV+CYP+MLT+ROU.B1_GE.HCPC/all?startTime=2010&endTime=2015&dimensionAtObservation=allDimensions"
   var requests = [d3.json(teensInViolentArea), d3.json(teenPregnancies), d3.json(gdp)];
 
   Promise.all(requests).then(function(response) {
@@ -16,14 +16,70 @@ window.onload = function() {
 
 // Converts data to a usable format
 function convertToUsableData(data){
-  dataSetOne = data[0];
-  dataSetTwo = data[1];
-  dataSetThree = data[2];
-  console.log(dataSetOne["dataSets"][0]["series"]);
-  console.log(dataSetOne["structure"]["dimensions"]["series"][0]["values"]);
-  console.log(dataSetTwo["structure"]["dimensions"]["series"][0]["values"]);
-  console.log(dataSetOne["structure"]["dimensions"]["observation"][0]["values"]);
-  console.log(dataSetTwo["dataSets"][0]["series"])
-  console.log(dataSetThree["dataSets"][0]["observations"])
-  console.log(dataSetThree)
+  var firstDataCountries = [];
+  var firstDataValues = [];
+  var secondDataCountries = [];
+  var secondDataValues = [];
+  var thirdDataCountries = [];
+  var thirdDataValues = [];
+
+  // Extracts country names from first file
+  for (country in data[0]["structure"]["dimensions"]["series"][0]["values"]) {
+    firstDataCountries.push(data[0]["structure"]["dimensions"]["series"][0]["values"][country]["name"]);
+  }
+  console.log(firstDataCountries)
+
+  // Extracts data from first file and cleans it
+  temp_storage = [];
+  for (value in data[0]["dataSets"][0]["series"]) {
+    temp_storage.push(data[0]["dataSets"][0]["series"][value]["observations"]);
+  };
+  for (filthyData in temp_storage){
+    intermediate = [];
+    for (observation in temp_storage[filthyData]){
+      intermediate.push(temp_storage[filthyData][observation][0])
+    }
+    firstDataValues.push(intermediate)
+  }
+  console.log(firstDataValues)
+
+  // Extracts country names from second file
+  for (country in data[1]["structure"]["dimensions"]["series"][0]["values"]) {
+    secondDataCountries.push(data[1]["structure"]["dimensions"]["series"][0]["values"][country]["name"])
+  };
+  console.log(secondDataCountries);
+
+  // Extracts data from second file and cleans it
+  temp_storage = [];
+  for (value in data[1]["dataSets"][0]["series"]) {
+    temp_storage.push(data[1]["dataSets"][0]["series"][value]["observations"]);
+  };
+  for (filthyData in temp_storage){
+    intermediate = [];
+    for (observation in temp_storage[filthyData]){
+      intermediate.push(temp_storage[filthyData][observation][0])
+    }
+    secondDataValues.push(intermediate)
+  }
+  console.log(secondDataValues)
+
+  // Extracts country names from third file
+  for (country in data[2]["structure"]["dimensions"]["observation"][0]["values"]) {
+    thirdDataCountries.push(data[2]["structure"]["dimensions"]["observation"][0]["values"][country]["name"])
+  };
+  console.log(thirdDataCountries)
+
+  // Extracts data from third file and cleans it
+  intermediate = [];
+  var counter = 0
+  for (value in data[2]["dataSets"][0]["observations"]){
+    intermediate.push(data[2]["dataSets"][0]["observations"][value][0])
+    counter += 1
+    if (counter > 5){
+      thirdDataValues.push(intermediate)
+      counter = 0;
+      intermediate = [];
+    }
+  }
+  console.log(thirdDataValues)
 };
