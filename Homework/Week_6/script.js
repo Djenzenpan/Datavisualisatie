786fc5c576284ff557e149bf9fe321b865b6ebf9
Djenzenpan <jmppannekeet@gmail.com>
2019-05-29 23:30:55 +0200

    Comments

--- Homework/Week_6/script.js
+++ Homework/Week_6/script.js
@@ -27,6 +27,9 @@ window.onload = function() {
       // Gives information to user
       information();
 
+      // Sets body to centre
+      d3v5.selectAll("body").style("text-align", "centre")
+
   }}
   txtFile.open("GET", fileName);
   txtFile.send();
@@ -48,6 +51,7 @@ function information() {
 
 }
 
+// Plots worldmap based on data
 function worldMap(allData) {
 
   // Creates div for the datamap to reside in
@@ -62,91 +66,90 @@ function worldMap(allData) {
   // Gets neccesary info from JSON file
   series = []
   for (country in datafile) {
-    series.push([datafile[country]["Country code"], parseInt(datafile[country]["Happy Planet Index"])])
+    series.push([datafile[country]["Country code"],
+    parseInt(datafile[country]["Happy Planet Index"])])
   }
 
-  //
+  // Finds specified values and their minimum and maximum
   var dataset = {};
-  var onlyValues = series.map(function(obj){ return parseFloat(obj[1]); });
-  var minValue = Math.min.apply(null, onlyValues),
-          maxValue = Math.max.apply(null, onlyValues);
+  var happy = series.map(function(obj){ return parseFloat(obj[1]); });
+  var minValue = Math.min.apply(null, happy),
+      maxValue = Math.max.apply(null, happy);
 
-  // Adds Colour palette
+  // Adds Colour palette based on minimum and maximum
   var paletteScale = d3v3.scale.linear()
-          .domain([minValue,maxValue])
-          .range(["#ece7f2","#2b8cbe"]); // blue color
+                         .domain([minValue,maxValue])
+                         .range(["#ece7f2","#2b8cbe"]); // blue color
 
   // fill dataset in appropriate format
   series.forEach(function(item){ //
       // item example value ["USA", 70]
       var iso = item[0],
               value = item[1]
-      dataset[iso] = { numberOfThings: value, fillColor: paletteScale(value) };
+      dataset[iso] = { HappyPlanetIndexThings: value, fillColor: paletteScale(value) };
   });
 
-  // render map
+  // renders map based on dataset
   new Datamap({
-      element: document.getElementById('container'),
-      projection: 'mercator', // big world map
-      // countries don't listed in dataset will be painted with this color
-      fills: { defaultFill: '#F5F5F5' },
-      data: dataset,
-      done: function(data) {
-          data.svg.selectAll('.datamaps-subunit').on('click', function(geo) {
-              scatterPlot(geo["id"], datafile) })},
-      geographyConfig: {
-          borderColor: '#000000',
-          highlightBorderWidth: 2,
-          // don't change color on mouse hover
-          highlightFillColor: function(geo) {
-              return '#FEFEFE';
-          },
-          // only change border
-          highlightBorderColor: '#B7B7B7',
-          // show desired information in tooltip
-          popupTemplate: function(geo, data) {
-              // don't show tooltip if country don't present in dataset
-              if (!data) { return ['<div class="hoverinfo">',
-                  '<strong>', geo.properties.name, '</strong>',
-                  '<br>No data available <strong>', '</div>'].join(''); }
-              // tooltip content
-              return ['<div class="hoverinfo">',
-                  '<strong>', geo.properties.name, '</strong>',
-                  '<br>Happy planet index: <strong>', data.numberOfThings, '</strong>',
-                  '</div>'].join('');},
-
-                }});
+    element: document.getElementById('container'),
+    projection: 'mercator',
+    fills: { defaultFill: '#F5F5F5' },
+    data: dataset,
+    done: function(data) {
+      data.svg.selectAll('.datamaps-subunit').on('click', function(geo) {
+          scatterPlot(geo["id"], datafile) })},
+    geographyConfig: {
+      borderColor: '#000000',
+      highlightBorderWidth: 2,
+      highlightFillColor: function(geo) {
+        return '#FEFEFE';
+        },
+      highlightBorderColor: '#B7B7B7',
+      popupTemplate: function(geo, data) {
+        if (!data) { return ['<div class="hoverinfo">',
+          '<strong>', geo.properties.name, '</strong>',
+          '<br>No data available <strong>', '</div>'].join(''); }
+        return ['<div class="hoverinfo">',
+          '<strong>', geo.properties.name, '</strong>',
+          '<br>Happy planet index: <strong>', data.HappyPlanetIndex, '</strong>',
+          '</div>'].join('');
+      }
+    }
+  });
 }
 
+// Plots scatterplot based on region that clicked country is in
 function scatterPlot(country, allData) {
 
-  d3v5.selectAll("#scatter").remove();
-  d3v5.selectAll("body").style("text-align", "centre")
+  // Removes existing scatterplots
+  d3v5.selectAll("#scatter")
+      .remove();
+
+  // Sets width and height of scatterplot
   var w = 850;
   var h = 450;
 
+  // Finds region associated with specified country
   for (i in allData) {
     if (allData[i]["Country code"] == country) {
       continent = allData[i]["Region"]
-    }
-  }
+    };
+  };
 
+  // Only remembers regions that specified country is in
   data = [];
-
   for (i in allData) {
     if (allData[i]["Region"] == continent) {
       data.push(allData[i])
-    }
-  }
+    };
+  };
 
-//  var tip = d3v5.tip()
-  //.attr('class', 'd3-tip')
-  //.offset([-10, 0])
-  //.html(data, function(d) {
-  //  return "You're touching my!";
-  //});
-
-  var svg = d3v5.select("body").append("svg").attr("id", "scatter").attr("width", w).attr("height", h);
+  // Adds svg for scatterplot to reside in
+  var svg = d3v5.select("body")
+                .append("svg")
+                .attr("id", "scatter")
+                .attr("width", w)
+                .attr("height", h);
 
 
   var tip = d3v5.select('#scatter')
@@ -164,64 +167,78 @@ function scatterPlot(country, allData) {
                   tip.style('display', 'none');
                 });
 
-  // Sets scale
+  // Sets x and y scales
   var xScale = d3v5.scaleLinear()
-                 .domain([0, d3v5.max(data, function(d) { return d["Happy Planet Index"]; })])
+                 .domain([0, d3v5.max(data, function(d) {
+                 return d["Happy Planet Index"]; })])
                  .range([10, w - 50]);
-  // Sets scale
   var yScale = d3v5.scaleLinear()
-                 .domain([0, d3v5.max(data, function(d) { return parseInt(d["Happy Life Years"]); })])
+                 .domain([0, d3v5.max(data, function(d) {
+                 return parseInt(d["Happy Life Years"]); })])
                  .range([50, h - 50]);
 
   // Sets x and y axis range
   var xAxis = d3v5.axisBottom(d3v5.scaleLinear()
-                              .domain([0, d3v5.max(data, function(d) {
-                                                           return d["Happy Planet Index"]; })])
-                              .range([10, w - 50]));
+                  .domain([0, d3v5.max(data, function(d) {
+                  return d["Happy Planet Index"]; })])
+                  .range([10, w - 50]));
   var yAxis = d3v5.axisLeft(d3v5.scaleLinear()
-                            .domain([0, d3v5.max(data, function(d) {
-                                                        return parseInt(d["Happy Life Years"]); })])
-                            .range([h - 50, 50]));
+                  .domain([0, d3v5.max(data, function(d) {
+                  return parseInt(d["Happy Life Years"]); })])
+                  .range([h - 50, 50]));
 
+  // Sets title of scatterplot
+  svg.append("text")
+     .attr("transform", "translate(410, 40)")
+     .style("text-anchor", "middle")
+     .style("font-family", "Arial")
+     .style("font-size", "20px")
+     .text("Happy planet index/happy life years (" + continent + ")");
 
-  // Sets title
-  svg.append("text").attr("transform", "translate(410, 40)")
-     .style("text-anchor", "middle").style("font-family", "Arial")
-     .style("font-size", "20px").text("Happy planet index/happy life years (" + continent + ")");
-
-  // Sets axes
-  svg.append("g").attr("class", "axis").call(xAxis)
+  // Sets axes of scatterplot
+  svg.append("g")
+     .attr("class", "axis")
+     .call(xAxis)
      .attr("transform", "translate(35, 400)");
-  svg.append("g").attr("class", "axis").call(yAxis)
+  svg.append("g")
+     .attr("class", "axis")
+     .call(yAxis)
      .attr("transform", "translate(45, 0)");
 
-  // Sets axes labels
-  svg.append("text").attr("transform", "translate(410, 435)")
-     .style("text-anchor", "middle").style("font-family", "Arial")
+  // Sets axes labels of scatterplot
+  svg.append("text")
+     .attr("transform", "translate(410, 435)")
+     .style("text-anchor", "middle")
+     .style("font-family", "Arial")
      .style("font-size", "15px")
      .text("Happy Planet Index");
-  svg.append("text").attr("transform", "rotate(-90)").attr("y", 15)
-     .attr("x", -205).style("text-anchor", "middle")
-     .style("font-family", "Arial").style("font-size", "15px")
+  svg.append("text")
+     .attr("transform", "rotate(-90)")
+     .attr("y", 15)
+     .attr("x", -205)
+     .style("text-anchor", "middle")
+     .style("font-family", "Arial")
+     .style("font-size", "15px")
      .text("Happy Life Years");
 
   // Plots datacircles
-  svg.selectAll("circle").data(data).enter().append("circle")
-    .attr("cx", function(d){ return xScale(d["Happy Planet Index"]); })
-    .attr("cy", function(d){ return yScale(parseInt(d["Happy Life Years"])) - 10; })
-    .style("fill", "000000")
-    .attr("r", "5px")
-    .on('mouseover', function(d, i) {
+  svg.selectAll("circle")
+     .data(data)
+     .enter()
+     .append("circle")
+     .attr("cx", function(d){ return xScale(d["Happy Planet Index"]); })
+     .attr("cy", function(d){ return yScale(parseInt(d["Happy Life Years"])) - 10; })
+     .style("fill", "000000")
+     .attr("r", "5px")
+     .on('mouseover', function(d, i) {
         tip.transition().duration(0);
         tip.style('top', "-400");
         tip.style('left', "-400");
         tip.style('display', 'block');
-      })
-      .on('mouseout', function(d, i) {
+     })
+     .on('mouseout', function(d, i) {
         tip.transition()
         .delay(500)
         .style('display', 'none');
-      });
-
-
+    });
 }
